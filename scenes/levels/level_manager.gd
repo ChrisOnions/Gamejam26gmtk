a01893b11b61ba0b9a188c07c0c3a1fa4b3e7dcf
j1a2a3a4 <jensantonandresen@gmail.com>
2026-07-23 14:22:10 +0200

    exits to next level 1/2 done

--- scenes/levels/level_manager.gd
+++ scenes/levels/level_manager.gd
@@ -1,10 +1,12 @@
 extends Node2D
+class_name LevelManager
 
 @export var current_level:int = 0  # level o is the tutorial
 
 func _ready() -> void:
 	EventBus.load_level.connect(on_load_level)  # makes ti so when load_level is cal current_level is opptated
-
+	GameManager.level_manager = self
+	
 func _process(delta: float) -> void:
 	pass
 	
