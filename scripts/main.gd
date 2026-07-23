fe318b36a75596c51df593da1314c7e6ddb6259e
Christopher Onions <c.onions101@gmail.com>
2026-07-23 14:03:53 +1000

    Basic Setup
    ok ,added some functionality to the game
    - The player spawns into main scene
    also added
    - singletons,
    - main scene,
    - event bus script,
    - custom input map.

---
+++ scripts/main.gd
@@ -0,0 +1,10 @@
+extends Node
+const PLAYER_2D = preload("uid://bts4on5khvdh2")
+
+func _ready() -> void:
+	Spawn_Player()
+	pass
+
+func Spawn_Player():
+	var player = PLAYER_2D.instantiate()
+	add_child(player)
