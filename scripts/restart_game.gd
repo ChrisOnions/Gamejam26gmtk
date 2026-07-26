06b6edaa75bf7212b9181ff72e5ce76093c22cf4
KramchayDig <m4tid@live.com.ph>
2026-07-26 23:23:10 +0800

    game over UI update.

--- scripts/restart_game.gd
+++ scripts/restart_game.gd
@@ -8,5 +8,12 @@ func _ready() -> void:
 	##EventBus.player_died.connect()
 
 func onbuttonpressed() -> void:
-	EventBus.load_level.emit(player.current_level)
-	queue_free()
+	get_tree().reload_current_scene()
+	#EventBus.load_level.emit(player.current_level)
+	#queue_free()
+
+
+func _on_exit_b_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
+	#get_tree().change_scene_to_packed(start_screen)
+	pass # Replace with function body.
