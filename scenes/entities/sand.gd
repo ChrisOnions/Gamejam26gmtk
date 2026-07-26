6c2bb9ac53167c7c89441e24cd6429675342c3e6
Kwamena - AfroNord <Kwamena.brace@gmail.com>
2026-07-26 21:37:45 +1000

    sfx waterfall, sand, key pickup

--- scenes/entities/sand.gd
+++ scenes/entities/sand.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+@onready var sfx_powerup: AudioStreamPlayer = $sfx_powerup
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is PLAYER:
 		body.add_sand(1.0)
 		print("New Max Capacity: ", body.max_capacity)
+		sfx_powerup.play()
+		await sfx_powerup.finished
 		queue_free()
--- scenes/levels/key.gd
+++ scenes/levels/key.gd
@@ -1,8 +1,11 @@
 extends Sprite2D
 
-
+@onready var sfx_pickup: AudioStreamPlayer = $sfx_key
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is PLAYER:
 		body.has_key = true
+		
+		sfx_pickup.play()
+		await sfx_pickup.finished
 		self.queue_free()
